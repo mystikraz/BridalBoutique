7d69afa068bb96af4f00b6d33d3d2d976821a264
raj tandukar <royal_raj@outlook.com>
2017-01-21 13:52:59 +0545

    add navcontroller

--- OnlineShoppingStore.WebUI/Controllers/ProductController.cs
+++ OnlineShoppingStore.WebUI/Controllers/ProductController.cs
@@ -16,11 +16,12 @@ namespace OnlineShoppingStore.WebUI.Controllers
         {
             repository = repo;
         }
-        public ViewResult List( int page=1)
+        public ViewResult List(string category, int page=1)
         {
             ProductsListViewModel model = new ProductsListViewModel
             {
                 Products = repository.Products
+                .Where(p=>category==null||p.Category==category)
                 .OrderBy(p => p.ProductID)
                 .Skip((page - 1) * PageSize)
                 .Take(PageSize),
@@ -29,7 +30,8 @@ namespace OnlineShoppingStore.WebUI.Controllers
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
                     TotalItems = repository.Products.Count()
-                }
+                },
+                CurrentCategory=category
             };
             return View(model);
         }
