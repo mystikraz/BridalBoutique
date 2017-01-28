c914f2d50b5774b1a2e8e5d5d8ad650f2966a76a
raj tandukar <royal_raj@outlook.com>
2017-01-28 16:07:50 +0545

    iOrderprocessor and EmailOrderProcessor

---
+++ OnlineShoppingStore.Domain/Abstract/IOrderProcessor.cs
@@ -0,0 +1,11 @@
+﻿using OnlineShoppingStore.Domain.Entities;
+
+namespace OnlineShoppingStore.Domain.Abstract
+{
+    public interface IOrderProcessor
+    {
+
+        void ProcessOrder(Cart cart, ShippingDetails shippingDetails);
+    }
+
+}
