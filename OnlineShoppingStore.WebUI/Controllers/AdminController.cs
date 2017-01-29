e9e6b5d5f85d3da0e6aa5186ce6ed8260bb39660
raj tandukar <royal_raj@outlook.com>
2017-01-29 13:48:31 +0545

    added admin controller

---
+++ OnlineShoppingStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using OnlineShoppingStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShoppingStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
